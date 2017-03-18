22c87615e88bc0e2c25393aa43d64c8bd37d87e2
Erik Lindsay <erik.lindsay@uconn.edu>
2017-03-18 17:33:22 -0400

    added image preview

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+    
+    
+    
+    $( "article img" ).click(function() {
+        var src = $(this).attr("src");
+        $( "#big-pic" ).attr("src", src);
+        $( "div.big-img" ).toggleClass("on");
+        
+        $( "body" ).toggleClass("noscroll");
+    });
+    
+    $( "div.big-img, #close" ).click(function(e) {
+        if (e.target !== this)
+            return;
+        $( "div.big-img" ).toggleClass("on");
+        $( "body" ).toggleClass("noscroll");
+    });
+    
+});
